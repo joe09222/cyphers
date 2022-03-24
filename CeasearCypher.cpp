b971c61f457e25a613d6821a427dc105c001e74f
joe <youasefmohsen425@outlook.com>
2022-03-24 16:32:48 +0200

    added ceaser cypher

---
+++ CeasearCypher.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void toLower( string& msg );
+string cesarCypher( string msg, int shiftSize );
+string cesarDeCypher( string msg, int shiftSize );
+
+
+int main() {
+
+
+
+}
+
+// chnage string to lower case 
+void toLower( string& msg ){
+    for( int i = 0 ; i < msg.length() ;i++){
+        msg[i] = tolower(msg[i]);
+    }
+}
+
+// 97 -> 122
+// shift right 
+
+
+string cesarCypher( string msg, int shiftSize ){
+    shiftSize = abs(shiftSize);
+    while(shiftSize >= 26){
+        shiftSize %= 26;
+    }
+    toLower(msg);
+    string cypherdMsg = "";
+
+    for( int i = 0; i < msg.length() ;i++){
+        int letterIndex = int(msg[i]);
+        if( letterIndex - shiftSize < 97){
+            int newLetterIndex = (letterIndex - shiftSize + 26);
+            cypherdMsg += char( newLetterIndex );
+        }else{
+            int newLetterIndex = letterIndex - shiftSize;
+            cypherdMsg += char( newLetterIndex );
+        }
+
+    }
+
+
+    return cypherdMsg;
+
+}
+
+string cesarDeCypher( string msg, int shiftSize ){
+    shiftSize = abs(shiftSize);
+    while(shiftSize >= 26){
+        shiftSize %= 26;
+    }
+    toLower(msg);
+    string originalMsg = "";
+
+    for( int i = 0; i < msg.length() ; i++){
+        int letterIndex = int(msg[i]);
+        if( letterIndex + shiftSize > 122){
+            int newLetterIndex = (letterIndex + shiftSize - 26);
+            originalMsg += char( newLetterIndex );
+        }else{
+            int newLetterIndex = (letterIndex + shiftSize);
+            originalMsg += char( newLetterIndex );
+        }
+    }
+
+
+
+
+    return originalMsg;
+
+}
